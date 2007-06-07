b6cb7aeb406892f1db0436f25dd38b5b6c08ced9
grempe <grempe@2e111c3a-5438-44ef-9c14-21a960333853>
2007-06-07 18:25:26 +0000

    r1583@macbook-pro:  glenn | 2007-06-06 23:40:12 -0700  major refactoring, response, image methods, instance methods, implemented LOTS of test/spec BDD tests for images and instances.  Still a way to go, but getting there.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/amazon-ec2/trunk@40 2e111c3a-5438-44ef-9c14-21a960333853

--- amazon-ec2/test/test_EC2.rb
+++ amazon-ec2/test/test_EC2.rb
@@ -6,43 +6,13 @@ class TestEC2 < Test::Unit::TestCase
     @ec2 = EC2::AWSAuthConnection.new('not a key', 'not a secret')
   end
   
-  # IMAGE_ATTRIBUTES
-  
-  
-  # IMAGES
   def test_register_image
-  end
-  
-  def test_deregister_image
   end
   
   def test_describe_images
   end
   
-  # INSTANCES
-  def test_reboot_instances
-    body = <<-RESPONSE
-    <RebootInstancesResponse xmlns="http://ec2.amazonaws.com/doc/2007-01-03/">
-        <return>true</return>
-    </RebootInstancesResponse>
-    RESPONSE
-    
-    @ec2.expects(:make_request).with('RebootInstances', {"InstanceId.1"=>"i-2ea64347", "InstanceId.2"=>"i-21a64348"}).
-          returns stub(:body => body, :is_a? => true)
-    assert_equal true, @ec2.reboot_instances('i-2ea64347', 'i-21a64348')
+  def test_deregister_image
   end
-  
-  
-  # KEYPAIRS
-  
-  
-  # RESPONSES
-  
-  
-  # SECURITY GROUPS
-  
-  
-  # VERSION
-  
   
 end
--- amazon-ec2/test/test_helper.rb
+++ amazon-ec2/test/test_helper.rb
@@ -1,12 +1,10 @@
-require 'test/unit'
-
-begin
-  require 'rubygems'
-  require 'mocha'
-  require 'stubba'
-rescue LoadError
-  abort "Unable to load some required gems needed to run tests!"
-end
+%w[ test/unit rubygems test/spec mocha stubba ].each { |f| 
+  begin
+    require f
+  rescue LoadError
+    abort "Unable to load required gem for test: #{f}"
+  end
+}
 
 require File.dirname(__FILE__) + '/../lib/EC2'
 
