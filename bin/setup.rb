c1920d40a376a89ca5c66a0d00fe26a9a8e18527
Glenn Rempe <glenn@rempe.us>
2009-01-31 10:55:37 -0800

    Whitespace

--- bin/setup.rb
+++ bin/setup.rb
@@ -10,7 +10,7 @@
 #++
 
 if ENV['AMAZON_ACCESS_KEY_ID'] && ENV['AMAZON_SECRET_ACCESS_KEY']
-  opts = { 
+  opts = {
     :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
   }
