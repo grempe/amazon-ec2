362c306afaf8eb1de861d49ffbb7b8157aa82ff9
Kevin Menard <nirvdrum@gmail.com>
2010-01-03 18:09:07 -0500

    Fixed launch specification namespacing.

--- lib/AWS/EC2/spot_instance_requests.rb
+++ lib/AWS/EC2/spot_instance_requests.rb
@@ -20,7 +20,7 @@ module AWS
       # @option options [optional,String] :key_name (nil) The name of the key pair.
       # @option options [optional,Array of Strings or String] :security_group (nil) Name of the security group(s).
       # @option options [optional,String] :user_data (nil) MIME, Base64-encoded user data.
-      # @option options [optional,String] :instance_type (nil) Specifies the instance type.
+      # @option options [optional,String] :instance_type ("m1.small") Specifies the instance type.
       # @option options [optional,String] :kernel_id (nil) The ID of the kernel to select.
       # @option options [optional,String] :ramdisk_id (nil) The ID of the RAM disk to select. Some kernels require additional drivers at launch. Check the kernel requirements for information on whether you need to specify a RAM disk and search for the kernel ID.
       # @option options [optional,String] :subnet_id (nil) Specifies the Amazon VPC subnet ID within which to launch the instance(s) for Amazon Virtual Private Cloud.
@@ -30,6 +30,7 @@ module AWS
       #
       def create_spot_instances_request( options = {} )
         options = { :instance_count => 1,
+                    :instance_type => 'm1.small',
                     :base64_encoded => false }.merge(options)
 
         raise ArgumentError, ":addressing_type has been deprecated." if options[:addressing_type]
@@ -40,30 +41,29 @@ module AWS
         params = {}
 
         if options[:security_group]
-          params.merge!(pathlist("SecurityGroup", options[:security_group]))
+          params.merge!(pathlist("LaunchSpecification.SecurityGroup", options[:security_group]))
         end
 
         if options[:block_device_mapping]
-          params.merge!(pathhashlist('BlockDeviceMapping', options[:block_device_mapping].flatten, {:device_name => 'DeviceName', :virtual_name => 'VirtualName', :ebs_snapshot_id => 'Ebs.SnapshotId', :ebs_volume_size => 'Ebs.VolumeSize', :ebs_delete_on_termination => 'Ebs.DeleteOnTermination' }))
+          params.merge!(pathhashlist('LaunchSpecification.BlockDeviceMapping', options[:block_device_mapping].flatten, {:device_name => 'DeviceName', :virtual_name => 'VirtualName', :ebs_snapshot_id => 'Ebs.SnapshotId', :ebs_volume_size => 'Ebs.VolumeSize', :ebs_delete_on_termination => 'Ebs.DeleteOnTermination' }))
         end
 
-        params["SpotPrice"]                         = options[:spot_price]
-        params["InstanceCount"]                     = options[:instance_count].to_s
-        params["Type"]                              = options[:type] unless options[:type].nil?
-        params["ValidFrom"]                         = options[:valid_from].to_s unless options[:valid_from].nil?
-        params["ValidUntil"]                        = options[:valid_until].to_s unless options[:valid_until].nil?
-        params["LaunchGroup"]                       = options[:launch_group] unless options[:launch_group].nil?
-        params["AvailabilityZoneGroup"]             = options[:availability_zone_group] unless options[:availability_zone_group].nil?
-        params["ImageId"]                           = options[:image_id] unless options[:image_id].nil?
-        params["KeyName"]                           = options[:key_name] unless options[:key_name].nil?
-        params["UserData"]                          = user_data unless user_data.nil?
-        params["AddressingType"]                    = options[:address_type] unless options[:address_type].nil?
-        params["InstanceType"]                      = options[:instance_type] unless options[:instance_type].nil?
-        params["KernelId"]                          = options[:kernel_id] unless options[:kernel_id].nil?
-        params["RamdiskId"]                         = options[:ramdisk_id] unless options[:ramdisk_id].nil?
-        params["SubnetId"]                          = options[:subnet_id] unless options[:subnet_id].nil?
-        params["Placement.AvailabilityZone"]        = options[:availability_zone] unless options[:availability_zone].nil?
-        params["Monitory.Enabled"]                  = options[:monitoring_enabled].to_s unless options[:monitoring_enabled].nil?
+        params["SpotPrice"]                                             = options[:spot_price]
+        params["InstanceCount"]                                         = options[:instance_count].to_s
+        params["Type"]                                                  = options[:type] unless options[:type].nil?
+        params["ValidFrom"]                                             = options[:valid_from].to_s unless options[:valid_from].nil?
+        params["ValidUntil"]                                            = options[:valid_until].to_s unless options[:valid_until].nil?
+        params["LaunchGroup"]                                           = options[:launch_group] unless options[:launch_group].nil?
+        params["AvailabilityZoneGroup"]                                 = options[:availability_zone_group] unless options[:availability_zone_group].nil?
+        params["LaunchSpecification.ImageId"]                           = options[:image_id] unless options[:image_id].nil?
+        params["LaunchSpecification.KeyName"]                           = options[:key_name] unless options[:key_name].nil?
+        params["LaunchSpecification.UserData"]                          = user_data unless user_data.nil?
+        params["LaunchSpecification.InstanceType"]                      = options[:instance_type] unless options[:instance_type].nil?
+        params["LaunchSpecification.KernelId"]                          = options[:kernel_id] unless options[:kernel_id].nil?
+        params["LaunchSpecification.RamdiskId"]                         = options[:ramdisk_id] unless options[:ramdisk_id].nil?
+        params["LaunchSpecification.SubnetId"]                          = options[:subnet_id] unless options[:subnet_id].nil?
+        params["LaunchSpecification.Placement.AvailabilityZone"]        = options[:availability_zone] unless options[:availability_zone].nil?
+        params["LaunchSpecification.Monitoring.Enabled"]                = options[:monitoring_enabled].to_s unless options[:monitoring_enabled].nil?
         
         return response_generator(:action => "RequestSpotInstances", :params => params)
       end
