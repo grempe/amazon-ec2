317542fdac554bb8d7af99a33f11b48972c983fa
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-02-12 22:01:01 +0100

    Get rid of eventual trailing slash on EC2_URL variable.
    The Amazon-provided tools don't fail if the final slash is present in the
    variable, so amazon-ec2 shouldn't, either.

--- lib/AWS/EC2.rb
+++ lib/AWS/EC2.rb
@@ -9,7 +9,7 @@ module AWS
     if ENV['EC2_URL']
       EC2_URL = ENV['EC2_URL']
       VALID_HOSTS = ['https://ec2.amazonaws.com', 'https://us-east-1.ec2.amazonaws.com', 'https://us-west-1.ec2.amazonaws.com', 'https://eu-west-1.ec2.amazonaws.com']
-      raise ArgumentError, "Invalid EC2_URL environment variable : #{EC2_URL}" unless VALID_HOSTS.include?(EC2_URL)
+      raise ArgumentError, "Invalid EC2_URL environment variable : #{EC2_URL}" unless VALID_HOSTS.include?(EC2_URL.chomp('/'))
       DEFAULT_HOST = URI.parse(EC2_URL).host
     else
       # Default US API endpoint
