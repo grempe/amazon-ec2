a79484c6a23f7b84f62e2232eb49f3d2af9a3bc5
Glenn Rempe <glenn@rempe.us>
2010-06-10 14:37:49 -0700

    Stop checking for a specific list of hosts for EC2 endpoints when the user specifies a specific EC2_URL env var. User is responsible for specifying the correct endpoint.

--- lib/AWS/EC2.rb
+++ lib/AWS/EC2.rb
@@ -8,8 +8,6 @@ module AWS
     #   export EC2_URL='https://ec2.amazonaws.com'
     if ENV['EC2_URL']
       EC2_URL = ENV['EC2_URL']
-      VALID_HOSTS = ['https://ec2.amazonaws.com', 'https://us-east-1.ec2.amazonaws.com', 'https://us-west-1.ec2.amazonaws.com', 'https://eu-west-1.ec2.amazonaws.com']
-      raise ArgumentError, "Invalid EC2_URL environment variable : #{EC2_URL}" unless VALID_HOSTS.include?(EC2_URL.chomp('/'))
       DEFAULT_HOST = URI.parse(EC2_URL).host
     else
       # Default US API endpoint
